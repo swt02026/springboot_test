fa546242dc1ff0395a0e3a312bc3d7027a3027b6
swt02026 <swt02026@yahoo.com.tw>
2020-06-26 22:38:00 +0800

    1. add Delete method 2. add query parameter data class

--- src/main/java/com/example/demo/ProductController.kt
+++ src/main/java/com/example/demo/ProductController.kt
@@ -1,5 +1,4 @@
 package com.example.demo
-import  com.example.demo.Product
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -8,18 +7,18 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.net.URI
 
 @RestController
-@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(value = ["/products"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class ProductController {
     var productDB = mutableListOf<Product>(Product("aaaa","bbb",123))
-    @GetMapping("/products/{id}")
+    @GetMapping("{id}")
     fun getProduct(@PathVariable("id") id:String): ResponseEntity<Product> {
-        val product = productDB.find { p -> p.id == id } ?: return ResponseEntity.notFound().build<Product>()
+        val product = productDB.find { it.id == id } ?: return ResponseEntity.notFound().build<Product>()
         return ResponseEntity.ok().body(product)
     }
 
-    @PostMapping("/products")
+    @PostMapping
     fun postProduct(@RequestBody request: Product): ResponseEntity<Product> {
-        if(productDB.any { p -> p.id == request.id }){
+        if(productDB.any { it.id == request.id }){
             return ResponseEntity.status(HttpStatus.CONFLICT).build<Product>()
         }
         productDB.add(request.copy())
@@ -30,15 +29,40 @@ class ProductController {
         return ResponseEntity.created(location).body(request)
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("{id}")
     fun putProduct(@PathVariable("id") id: String, @RequestBody request: Product): ResponseEntity<Product> {
 
         // kotlin find/first can't get reference of data class
-        var product_index = productDB.indexOfFirst { p->p.id == id }
+        var product_index = productDB.indexOfFirst { it.id == id }
         if(product_index < 0) {
             return ResponseEntity.notFound().build<Product>()
         }
         productDB[product_index] = request
         return ResponseEntity.ok(request)
+    }
+
+    @DeleteMapping("{id}")
+    fun deleteProduct(@PathVariable("id") id: String): ResponseEntity<Product> {
+        val removed = productDB.removeIf { it.id == id}
+        if(removed){
+            return ResponseEntity.noContent().build<Product>()
+        }
+        return ResponseEntity.notFound().build<Product>()
+    }
+
+    @GetMapping
+    fun getProducts(@ModelAttribute param: ProductQueryParameter): ResponseEntity<List<Product>> {
+        val (keyword, orderBy, sortRule) = param
+        val productsFiltered = productDB.filter { keyword == null || keyword?.toUpperCase().toString() in it.name.toUpperCase() }
+        val productSorted = when(orderBy){
+            "name" ->productsFiltered.sortedBy { it.name }
+            "price" -> productsFiltered.sortedBy { it.price }
+            else -> productsFiltered
+        }
+        val productOrderSeq = when(sortRule) {
+            "desc" -> productSorted.reversed()
+            else -> productSorted
+        }
+        return ResponseEntity.ok(productOrderSeq)
     }
 }
---
+++ src/main/java/com/example/demo/ProductQueryParameter.kt
@@ -0,0 +1,5 @@
+package com.example.demo
+
+data class ProductQueryParameter(val keyword: String?, val orderBy: String?, val sortRule:String?) {
+
+}
